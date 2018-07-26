2faa1b34cb8f6f5d614870f7b0f43f187764513d
ZhaiMengdong <429322003@qq.com>
2018-07-26 16:27:39 +0800

    add the joudgement of privileged

--- zun/api/controllers/v1/containers.py
+++ zun/api/controllers/v1/containers.py
@@ -283,7 +283,6 @@ class ContainersController(base.Controller):
         extra_spec = {}
         extra_spec['hints'] = container_dict.get('hints', None)
         extra_spec['pci_requests'] = pci_req
-        privileged = container_dict.pop('privileged')
         if container_dict.has_key('privileged'):
             privileged = container_dict.pop('privileged')
         else:
