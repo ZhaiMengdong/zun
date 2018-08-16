46dce10264aafe471ebd30a5ecece16cda3d2095
mengdong <429322003@qq.com>
2018-08-16 10:23:38 +0800

    db update

---
+++ zun/db/sqlalchemy/alembic/versions/2b129060baff_container_cpuset.py
@@ -0,0 +1,25 @@
+"""add_column
+
+Revision ID: 2b129060baff
+Revises: 5f97e44127d6
+Create Date: 2018-08-16 10:08:40.547664
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2b129060baff'
+down_revision = '5f97e44127d6'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('container',
+                  sa.Column('cpu_policy', sa.String(length=255)))
+    op.add_column('container',
+                  sa.Column('cpuset_cpus', sa.String(length=255)))
+    op.add_column('container',
+                  sa.Column('cpuset_mems', sa.String(length=255)))
